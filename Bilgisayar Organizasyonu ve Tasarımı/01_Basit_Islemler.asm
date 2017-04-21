075bcee81f285240d910ad323716c4883bd6030b
alperenbozkurt <bozkurttalperen@gmail.com>
2017-04-21 13:19:08 +0300

    Assembly input-output hatası düzeltildi.

--- Bilgisayar Organizasyonu ve Tasarımı/01_Basit_Islemler.asm
+++ Bilgisayar Organizasyonu ve Tasarımı/01_Basit_Islemler.asm
@@ -19,53 +19,63 @@ main:
   la      $a0, girdi1
 syscall
   li      $v0, 5
-  la      $a0, buyukx
-syscall
+  sw      $a0, buyukx
+  syscall
+  move $t0, $v0
+
   li      $v0, 5
   la      $a0, buyuky
-syscall
+  syscall
+  move $t1, $v0
+
   li      $v0, 5
   la      $a0, z
-syscall
+  syscall
+  move $t2, $v0
+
   li      $v0, 4
   la      $a0, girdi2
-syscall
+  syscall
+
   li      $v0, 5
   la      $a0, kucukx
-syscall
+  syscall
+  move $t3, $v0
+
   li      $v0, 5
   la      $a0, kucuky
-syscall
+  syscall
+  move $t4, $v0
+
   li      $v0, 4
   la      $a0, girdi3
-syscall
+  syscall
+
   li      $v0, 5
   la      $a0, yogunluk
-syscall
+  syscall
+  move $t5, $v0
+
   li      $v0, 4
   la      $a0, girdi4
-syscall
+  syscall
+
   li      $v0, 5
   la      $a0, birimFiyat
-syscall
-  lw $t0, buyukx
-  lw $t1, buyuky
-  lw $t2, z
-  lw $t3, kucukx
-  lw $t4, kucuky
-  mul $t5, $t0, $t1
-  mul $s0, $t5, $t2
+  syscall
+  move $t6, $v0
 
-  mul $t6, $t2, $t3
-  mul $s1, $t6, $t4
+  mul $t1, $t0, $t1
+  mul $s0, $t1, $t2
+
+  mul $t3, $t2, $t3
+  mul $s1, $t3, $t4
 
   sub $s2, $s0, $s1
 
-  lw $t7, yogunluk
-  mul $s3, $s2, $t7
+  mul $s3, $s2, $t5
 
-  lw $t8, birimFiyat
-  mul $s4, $s3, $t8
+  mul $s4, $s3, $t6
 
   sw $s4, cevap
 
